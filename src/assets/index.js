d4b07af3fdd5c85114f3ec90dce58122b03783ee
Azlan Raban <azlanshaveenraban1@gmail.com>
2025-11-20 21:47:01 +0530

    Feat: New up to date design

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,23 +2,23 @@ import insta from "./social/insta.svg";
 import github from "./social/github.svg";
 import linkedin from "./social/linkedin.svg";
 import info from "./social/info.svg";
-import SolarFire from "./images/solar_fire.svg";
+import SolarFire from "./Images/solar_fire.svg";
 
 import JavaScript from "./skills/JavaScript.svg";
-import Java from "./skills/Java.svg";
-import Python from "./skills/Python.svg";
-import C from "./skills/C.svg";
-import CPlusPlus from "./skills/CPlusPlus.svg";
-import CSS3 from "./skills/CSS3.svg";
-import HTML5 from "./skills/HTML5.svg";
-import ReactIcon from "./skills/React.svg";
-import Nodejs from "./skills/Nodejs.svg";
-import TailwindCSS from "./skills/TailwindCSS.svg";
-import Express from "./skills/Express.svg";
-import MongoDB from "./skills/MongoDB.svg";
-import Figma from "./skills/Figma.svg";
-import Docker from "./skills/Docker.svg";
-import Git from "./skills/Git.svg";
+import Java from "./skills/java.svg";
+import Python from "./skills/python.svg";
+import C from "./skills/c.svg";
+import CPlusPlus from "./skills/cplusplus.svg";
+import CSS3 from "./skills/css3.svg";
+import HTML5 from "./skills/html5.svg";
+import ReactIcon from "./skills/react.svg";
+import Nodejs from "./skills/nodejs.svg";
+import TailwindCSS from "./skills/tailwindcss.svg";
+import Express from "./skills/express.svg";
+import MongoDB from "./skills/mongodb.svg";
+import Figma from "./skills/figma.svg";
+import Docker from "./skills/docker.svg";
+import Git from "./skills/git.svg";
 
 export {
   insta,
